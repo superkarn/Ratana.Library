7b84456df103a267e011ba81a3a10f0f1b477277
Karn Ratana <karn.ratana@gmail.com>
2015-08-21 00:42:05 -0400

    Add ICache interface with InMemoryCache, NoCache implementations.

---
+++ RatanaLibrary.Common/Cache/NoCache.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RatanaLibrary.Common.Cache
+{
+    /// <summary>
+    /// An implementation of the ICache interface that does not do anything.
+    /// This is useful for testing.
+    /// </summary>
+    public class NoCache : ICache
+    {
+        T ICache.GetOrAdd<T>(String key, Func<T> orAdd)
+        {
+            return ((ICache)this).GetOrAdd(key, orAdd, TimeSpan.FromDays(1));
+        }
+
+        T ICache.GetOrAdd<T>(String key, Func<T> orAdd, TimeSpan expiration)
+        {
+            return orAdd();
+        }
+
+        void ICache.Remove(String key)
+        {
+            // Nothing to do
+        }
+    }
+}
