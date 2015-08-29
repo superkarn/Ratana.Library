fd733d5db199c47c9afe6ef2f83025032df4a271
Karn Ratana <karn.ratana@gmail.com>
2015-08-28 23:03:37 -0400

    Add LoggerExtentions. Rename ILog.cs to ILogger.cs. Remove unused references.

--- RatanaLibrary.Common/Log/LoggerExtentions.cs
+++ RatanaLibrary.Common/Log/LoggerExtentions.cs
@@ -1,21 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RatanaLibrary.Common.Log
 {
     public static class LoggerExtentions
     {
-        public static void Log(this ILogger logger, string message)
+        public static void Verbose(this ILogger logger, string message, string source = null, Exception exception = null)
         {
-            logger.Log(new LogEntry(LoggingEventType.Information, message, null, null));
+            logger.Log(new LogEntry(LoggingEventType.Verbose, message, source, exception));
         }
 
-        public static void Log(this ILogger logger, Exception exception)
+        public static void Debug(this ILogger logger, string message, string source = null, Exception exception = null)
         {
-            logger.Log(new LogEntry(LoggingEventType.Error, exception.Message, null, exception));
+            logger.Log(new LogEntry(LoggingEventType.Debug, message, source, exception));
+        }
+
+        public static void Information(this ILogger logger, string message, string source = null, Exception exception = null)
+        {
+            logger.Log(new LogEntry(LoggingEventType.Information, message, source, exception));
+        }
+
+        public static void Warning(this ILogger logger, string message, string source = null, Exception exception = null)
+        {
+            logger.Log(new LogEntry(LoggingEventType.Warning, message, source, exception));
+        }
+
+        public static void Error(this ILogger logger, string message, string source = null, Exception exception = null)
+        {
+            logger.Log(new LogEntry(LoggingEventType.Error, message, source, exception));
+        }
+
+        public static void Fatal(this ILogger logger, string message, string source = null, Exception exception = null)
+        {
+            logger.Log(new LogEntry(LoggingEventType.Fatal, message, source, exception));
+        }
+
+
+
+        /// <summary>
+        /// Log with specified severity.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="severity"></param>
+        /// <param name="message"></param>
+        /// <param name="source"></param>
+        /// <param name="exception"></param>
+        public static void Log(this ILogger logger, LoggingEventType severity, string message, string source = null, Exception exception = null)
+        {
+            logger.Log(new LogEntry(severity, message, source, exception));
         }
     }
 }
