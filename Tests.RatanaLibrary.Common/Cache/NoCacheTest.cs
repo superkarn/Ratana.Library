4a2b5d52d018bd21d61df951145ec0569ec95954
Karn Ratana <karn.ratana@gmail.com>
2015-08-29 01:25:21 -0400

    Add blank NoCacheTest and InMemoryCacheTest.

---
+++ Tests.RatanaLibrary.Common/Cache/NoCacheTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests.RatanaLibrary.Common.Cache
+{
+    [TestFixture]
+    public class NoCacheTest
+    {
+        [Test]
+        public void GetOrAdd()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Remove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
