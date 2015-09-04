0e3260ddb177a03b98998f0463c0b5a7f1b10a91
Karn Ratana <karn.ratana@gmail.com>
2015-09-03 23:56:59 -0400

    Add InMemoryCacheTest.GetOrAdd()

--- Tests.RatanaLibrary.Common/Cache/InMemoryCacheTest.cs
+++ Tests.RatanaLibrary.Common/Cache/InMemoryCacheTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using RatanaLibrary.Common.Cache;
 using System;
 
 namespace Tests.RatanaLibrary.Common.Cache
@@ -9,7 +10,44 @@ namespace Tests.RatanaLibrary.Common.Cache
         [Test]
         public void GetOrAdd()
         {
-            throw new NotImplementedException();
+            #region Arrange
+            // Set up some variables
+            var cacheKey = "test-key";
+            var cacheValue = "test-value";
+            var fakeValue = "test-fake-value";
+            var cache = new InMemoryCache();
+
+            // Make sure the key we're about to test is empty
+            ((ICache)cache).Remove(cacheKey);
+            #endregion
+
+
+            #region Act
+            // 1 Try to save cacheValue under cacheKey.
+            //   Since this key is new, the cacheValue should be saved to the cache
+            //   and returned to returnedCachValue1;
+            var returnedCachValue1 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+                {
+                    return cacheValue;
+                });
+
+            // 2 Try to save fakeValue under cacheKey.
+            var returnedCachValue2 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+                {
+                    return fakeValue;
+                });
+            #endregion
+
+
+            #region Assert
+            // returnedCachValue1 should equal cacheValue because the cache is empty.
+            Assert.AreEqual(cacheValue, returnedCachValue1);
+
+            // returnedCachValue2 should equal cacheValue because the cache exists, 
+            // so fakeValue was never returned.
+            Assert.AreEqual(cacheValue, returnedCachValue2);
+            Assert.AreNotEqual(fakeValue, returnedCachValue2);
+            #endregion
         }
 
         [Test]
