db3c747687e1c18ca815dffa9c85398525ada675
Karn Ratana <karn.ratana@gmail.com>
2015-09-04 00:02:41 -0400

    Add InMemoryCacheTest.Remove()

--- Tests.RatanaLibrary.Common/Cache/InMemoryCacheTest.cs
+++ Tests.RatanaLibrary.Common/Cache/InMemoryCacheTest.cs
@@ -53,7 +53,44 @@ namespace Tests.RatanaLibrary.Common.Cache
         [Test]
         public void Remove()
         {
-            throw new NotImplementedException();
+            #region Arrange
+            // Set up some variables
+            var cacheKey = "test-key";
+            var cacheValue1 = "test-value-1";
+            var cacheValue2 = "test-value-2";
+            var cache = new InMemoryCache();
+
+            // Make sure the key we're about to test is empty
+            ((ICache)cache).Remove(cacheKey);
+            #endregion
+
+
+            #region Act
+            // 1 Try to save cacheValue under cacheKey.
+            //   Since this key is new, the cacheValue should be saved to the cache
+            //   and returned to returnedCachValue1;
+            var returnedCachValue1 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+            {
+                return cacheValue1;
+            });
+
+            // 2 Remove the cacheKey
+            ((ICache)cache).Remove(cacheKey);
+
+            // 3 Try to save cacheValue2 under cacheKey again
+            var returnedCachValue2 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+            {
+                return cacheValue2;
+            });
+            #endregion
+
+
+            #region Assert
+            // returnedCachValue2 should equal cacheValue2 (but not cacheValue1)
+            // because the cache was empty the second time.
+            Assert.AreNotEqual(cacheValue1, returnedCachValue2);
+            Assert.AreEqual(cacheValue2, returnedCachValue2);
+            #endregion
         }
     }
 }
