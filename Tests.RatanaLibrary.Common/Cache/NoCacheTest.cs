5a046bab385ffa2afb11709d4f38b73c695e8b15
Karn Ratana <karn.ratana@gmail.com>
2015-09-15 00:08:18 -0400

    Add InMemoryCacheTest.GetOrAdd() Add InMemoryCacheTest.Remove()

--- Tests.RatanaLibrary.Common/Cache/NoCacheTest.cs
+++ Tests.RatanaLibrary.Common/Cache/NoCacheTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using RatanaLibrary.Common.Cache;
 using System;
 
 namespace Tests.RatanaLibrary.Common.Cache
@@ -9,13 +10,64 @@ namespace Tests.RatanaLibrary.Common.Cache
         [Test]
         public void GetOrAdd()
         {
-            throw new NotImplementedException();
+            #region Arrange
+            // Set up some variables
+            var cacheKey = "test-key";
+            var cacheValue1 = "test-value-1";
+            var cacheValue2 = "test-value-2";
+            var cache = new NoCache();
+            #endregion
+
+
+            #region Act
+            // 1 Try to save cacheValue1 under cacheKey.
+            var returnedCachValue1 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+            {
+                return cacheValue1;
+            });
+
+            // 2 Try to save cacheValue2 under cacheKey.
+            var returnedCachValue2 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+            {
+                return cacheValue2;
+            });
+            #endregion
+
+
+            #region Assert
+            // What we get back should equal what we put in, since there is no cache.
+            Assert.AreEqual(cacheValue1, returnedCachValue1);
+            Assert.AreEqual(cacheValue2, returnedCachValue2);
+            #endregion
         }
 
         [Test]
         public void Remove()
         {
-            throw new NotImplementedException();
+            #region Arrange
+            // Set up some variables
+            var cacheKey = "test-key";
+            var cacheValue1 = "test-value-1";
+            var cache = new NoCache();
+            #endregion
+
+
+            #region Act
+            // 1 Try to save cacheValue1 under cacheKey.
+            var returnedCachValue1 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+            {
+                return cacheValue1;
+            });
+
+            // 2 Remove the cacheKey
+            ((ICache)cache).Remove(cacheKey);
+            #endregion
+
+
+            #region Assert
+            // There is no cache.  There is nothing to assert.  
+            // Just make sure NoCache.Remove() doesn't throw an exception
+            #endregion
         }
     }
 }
