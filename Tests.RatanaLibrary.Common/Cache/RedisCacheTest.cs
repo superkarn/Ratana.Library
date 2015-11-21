10175fa9428c514c8d32d4890310d423d9d028c7
Karn Ratana <karn.ratana@gmail.com>
2015-11-21 15:22:26 -0500

    Add RedisCache implementation.

--- Tests.RatanaLibrary.Common/Cache/RedisCacheTest.cs
+++ Tests.RatanaLibrary.Common/Cache/RedisCacheTest.cs
@@ -8,14 +8,56 @@ namespace Tests.RatanaLibrary.Common.Cache
     public class RedisCacheTest
     {
         [Test]
-        public void GetOrAdd()
+        [TestCase("test-key1", "test-value", "test-fake-value")]
+        [TestCase("test-key2", "", "test-fake-value")]
+        public void GetOrAdd(string cacheKey, string cacheValue, string fakeValue)
+        {
+            #region Arrange
+            // Set up some variables
+            var cache = new RedisCache(new RedisCache.RedisSettings() { });
+
+            // Make sure the key we're about to test is empty
+            ((ICache)cache).Remove(cacheKey);
+            #endregion
+
+
+            #region Act
+            // 1 Try to save cacheValue under cacheKey.
+            //   Since this key is new, the cacheValue should be saved to the cache
+            //   and returned to returnedCachValue1;
+            var returnedCachValue1 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+            {
+                return cacheValue;
+            });
+
+            // 2 Try to save fakeValue under cacheKey.
+            var returnedCachValue2 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+            {
+                return fakeValue;
+            });
+            #endregion
+
+
+            #region Assert
+            // returnedCachValue1 should equal cacheValue because the cache is empty.
+            Assert.AreEqual(cacheValue, returnedCachValue1);
+
+            // returnedCachValue2 should equal cacheValue because the cache exists, 
+            // so fakeValue was never returned.
+            Assert.AreEqual(cacheValue, returnedCachValue2);
+            Assert.AreNotEqual(fakeValue, returnedCachValue2);
+            #endregion
+        }
+
+        [Test]
+        public void GetOrAddAnonymousType()
         {
             #region Arrange
             // Set up some variables
             var cacheKey = "test-key";
-            var cacheValue = "test-value";
-            var fakeValue = "test-fake-value";
-            var cache = new RedisCache();
+            var cacheValue = new { Name = "test-name", Value = "test-value" };
+            var fakeValue = new { Name = "test-fake-name", Value = "test-fake-value" };
+            var cache = new RedisCache(new RedisCache.RedisSettings() { });
 
             // Make sure the key we're about to test is empty
             ((ICache)cache).Remove(cacheKey);
@@ -58,7 +100,7 @@ namespace Tests.RatanaLibrary.Common.Cache
             var cacheKey = "test-key";
             var cacheValue1 = "test-value-1";
             var cacheValue2 = "test-value-2";
-            var cache = new InMemoryCache();
+            var cache = new RedisCache(new RedisCache.RedisSettings() { });
 
             // Make sure the key we're about to test is empty
             ((ICache)cache).Remove(cacheKey);
--- src/RatanaLibrary.Common/Cache/RedisCache.cs
+++ src/RatanaLibrary.Common/Cache/RedisCache.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RatanaLibrary.Common.Log;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace RatanaLibrary.Common.Cache
 {
@@ -7,43 +13,121 @@ namespace RatanaLibrary.Common.Cache
     /// </summary>
     public class RedisCache : ICache
     {
-        public RedisCache()
+        ConnectionMultiplexer redis;
+        IDatabase db;
+
+        private readonly TimeSpan DEFAULT_CACHE_DURATION = TimeSpan.FromDays(1);
+
+        public RedisCache(): this(new RedisSettings() { Server="localst" })
         {
         }
 
-        public RedisCache(RedisSettings settings)
+        public RedisCache(RedisSettings settings) : this(new List<RedisSettings> { settings })
         {
-
         }
 
-        T ICache.GetOrAdd<T>(string key, Func<T> orAdd)
+        public RedisCache(IList<RedisSettings> settings)
         {
-            throw new NotImplementedException();
+            // create the multiplexer
+            try
+            {
+                this.redis = ConnectionMultiplexer.Connect(
+                    String.Join(",", settings.Select(x => x.Server + ":" + x.Port))
+                );
+            }
+            catch (RedisConnectionException)
+            {
+                // do something here 
+                throw;
+            }
+
+            // use the database id from the first settings object
+            if (settings.FirstOrDefault() != null)
+            {
+                this.db = this.redis.GetDatabase(settings.FirstOrDefault().DatabaseId);
+            }
+            // if not available, use default db0
+            else
+            {
+                this.db = this.redis.GetDatabase(0);
+            }
         }
 
-        T ICache.GetOrAdd<T>(string key, Func<T> orAdd, TimeSpan expiration)
+        T ICache.GetOrAdd<T>(String key, Func<T> orAdd)
         {
-            throw new NotImplementedException();
+            return ((ICache)this).GetOrAdd(key, orAdd, this.DEFAULT_CACHE_DURATION);
         }
 
-        void ICache.Remove(string key)
+        T ICache.GetOrAdd<T>(String key, Func<T> orAdd, TimeSpan expiration)
         {
-            throw new NotImplementedException();
+            T value = default(T);
+
+            // Try to get the item
+            // If the item is not found, lock and try again.
+            // If it's not found this time, add it to the cache.
+            if (!this.TryGet(key, out value))
+            {
+                value = orAdd();
+
+                String json = RedisValue.Null;
+                if (value != null)
+                {
+                    json = JsonConvert.SerializeObject(value);
+                }
+                                    
+                this.db.StringSet(key, json, expiration);
+            }
+
+            return value;
+        }
+
+        void ICache.Remove(String key)
+        {
+            this.db.KeyDelete(key);
+        }
+
+        private String Get(String key)
+        {
+            return this.db.StringGet(key);
+        }
+
+        /// <summary>
+        /// Look for the item in the cache.
+        /// If the item is found, set the value and return true,
+        /// else set the out value to default and return false.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private Boolean TryGet<T>(String key, out T value)
+        {
+            String json = this.Get(key);
+
+            if (json == null)
+            {
+                value = default(T);
+                return false;
+            }
+
+            value = JsonConvert.DeserializeObject<T>(json);
+
+            return true;
         }
 
         public class RedisSettings
         {
-            public bool Enabled { get; }
+            public bool Enabled { get; set; }
 
-            public string Server { get; }
+            public String Server { get; set; }
 
-            public int Port { get; }
+            public int Port { get; set; }
 
-            public TimeSpan Retry { get; }
+            public TimeSpan Retry { get; set; }
 
-            public int DatabaseId { get; }
+            public int DatabaseId { get; set; }
 
-            public int PoolSizeMultiplier { get; }
+            public int PoolSizeMultiplier { get; set; }
         }
     }
 }
