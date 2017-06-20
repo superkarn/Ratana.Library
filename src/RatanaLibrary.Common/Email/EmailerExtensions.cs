265543035bc42840a6d3217fee6b67ff3e294bd2
Karn Ratana <karn.ratana@gmail.com>
2017-06-19 23:21:18 -0400

    Add Emailer and SystemNetEmailer

---
+++ src/RatanaLibrary.Common/Email/EmailerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Mail;
+
+namespace RatanaLibrary.Common.Email
+{
+    public static class EmailerExtensions
+    {
+        public static void Send(this IEmailer emailer, string fromAddress, string toAddress, string subject, string body)
+        {
+            var mailMessage = new MailMessage(fromAddress, toAddress, subject, body);
+            emailer.Send(mailMessage);
+        }
+    }
+}
