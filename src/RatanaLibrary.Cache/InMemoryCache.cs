7b61b2d37a8e932328221fc2260585797dbb98cd
Karn Ratana <karn.ratana@gmail.com>
2018-01-13 21:57:45 -0500

    Fix InMemoryCache, replace the implementation with Microsoft.Extensions.Caching.Memory

--- src/RatanaLibrary.Cache/InMemoryCache.cs
+++ src/RatanaLibrary.Cache/InMemoryCache.cs
@@ -1,68 +1,68 @@
-﻿//using System;
-//using System.Runtime.Caching;
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
 
-//namespace RatanaLibrary.Cache
-//{
-//    /// <summary>
-//    /// An in-memory implementation of the ICache interface.
-//    /// </summary>
-//    public class InMemoryCache : ICache
-//    {
-//        private static Object Lock = new Object();
+namespace RatanaLibrary.Cache
+{
+    /// <summary>
+    /// An in-memory implementation of the ICache interface.
+    /// </summary>
+    public class InMemoryCache : ICache
+    {
+        private IMemoryCache iMemoryCache;
+        private static Object Lock = new Object();
 
-//        public InMemoryCache()
-//        {
-//        }
+        public InMemoryCache()
+        {
+            MemoryCacheOptions options = new MemoryCacheOptions();
+            this.iMemoryCache = new MemoryCache(options);
+        }
 
-//        T ICache.GetOrAdd<T>(String key, Func<T> orAdd, TimeSpan expiration)
-//        {
-//            T value = default(T);
+        T ICache.GetOrAdd<T>(String key, Func<T> orAdd, TimeSpan expiration)
+        {
+            T value = default(T);
 
-//            // Try to get the item
-//            // If the item is not found, lock and try again.
-//            // If it's not found this time, add it to the cache.
-//            if (!this.TryGet(key, out value))
-//            {
-//                lock (InMemoryCache.Lock)
-//                {
-//                    if (!this.TryGet(key, out value))
-//                    {
-//                        value = orAdd();
-//                        MemoryCache.Default.Add(key, value, DateTime.Now.Add(expiration));
-//                    }
-//                }
-//            }
+            // Try to get the item
+            // If the item is not found, lock and try again.
+            // If it's not found this time, add it to the cache.
+            if (!this.TryGet(key, out value))
+            {
+                lock (InMemoryCache.Lock)
+                {
+                    if (!this.TryGet(key, out value))
+                    {
+                        value = orAdd();
 
-//            return value;
-//        }
+                        using (var entry = this.iMemoryCache.CreateEntry(key))
+                        {
+                            entry.Value = value;
+                            this.iMemoryCache.Set(key, value, DateTime.Now.Add(expiration));
+                        }
+                    }
+                }
+            }
 
-//        void ICache.Remove(String key)
-//        {
-//            MemoryCache.Default.Remove(key);
-//        }
+            return value;
+        }
+
+        void ICache.Remove(String key)
+        {
+            this.iMemoryCache.Remove(key);
+        }
 
 
 
-//        /// <summary>
-//        /// Look for the item in the cache.
-//        /// If the item is found, set the value and return true,
-//        /// else set the value to default and return false.
-//        /// </summary>
-//        /// <typeparam name="T"></typeparam>
-//        /// <param name="key"></param>
-//        /// <param name="value"></param>
-//        /// <returns></returns>
-//        private Boolean TryGet<T>(String key, out T value)
-//        {
-//            if (!MemoryCache.Default.Contains(key))
-//            {
-//                value = default(T);
-//                return false;
-//            }
-
-//            value = (T)MemoryCache.Default.Get(key);
-
-//            return true;
-//        }
-//    }
-//}
+        /// <summary>
+        /// Look for the item in the cache.
+        /// If the item is found, set the value and return true,
+        /// else set the value to default and return false.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private Boolean TryGet<T>(String key, out T value)
+        {
+            return this.iMemoryCache.TryGetValue<T>(key, out value);
+        }
+    }
+}
--- tests/Tests.RatanaLibrary/Cache/InMemoryCacheTest.cs
+++ tests/Tests.RatanaLibrary/Cache/InMemoryCacheTest.cs
@@ -1,97 +1,97 @@
-﻿//using NUnit.Framework;
-//using RatanaLibrary.Cache;
-//using Tests.RatanaLibrary.Attributes;
+﻿using NUnit.Framework;
+using RatanaLibrary.Cache;
+using Tests.RatanaLibrary.Attributes;
 
-//namespace Tests.RatanaLibrary.Common.Cache
-//{
-//    [TestFixture]
-//    public class InMemoryCacheTest
-//    {
-//        [Test]
-//        [Continuous, Integration]
-//        [TestCase("InMemoryCacheTest:GetOrAdd:test-key1", "test-value", "test-fake-value")]
-//        [TestCase("InMemoryCacheTest:GetOrAdd:test-key2", "", "test-fake-value")]
-//        public void GetOrAdd(string cacheKey, string cacheValue, string fakeValue)
-//        {
-//            #region Arrange
-//            // Set up some variables
-//            var cache = new InMemoryCache();
+namespace Tests.RatanaLibrary.Common.Cache
+{
+    [TestFixture]
+    public class InMemoryCacheTest
+    {
+        [Test]
+        [Continuous, Integration]
+        [TestCase("InMemoryCacheTest:GetOrAdd:test-key1", "test-value", "test-fake-value")]
+        [TestCase("InMemoryCacheTest:GetOrAdd:test-key2", "", "test-fake-value")]
+        public void GetOrAdd(string cacheKey, string cacheValue, string fakeValue)
+        {
+            #region Arrange
+            // Set up some variables
+            var cache = new InMemoryCache();
 
-//            // Make sure the key we're about to test is empty
-//            ((ICache)cache).Remove(cacheKey);
-//            #endregion
-
-
-//            #region Act
-//            // 1 Try to save cacheValue under cacheKey.
-//            //   Since this key is new, the cacheValue should be saved to the cache
-//            //   and returned to returnedCachValue1;
-//            var returnedCachValue1 = ((ICache)cache).GetOrAdd(cacheKey, () =>
-//                {
-//                    return cacheValue;
-//                });
-
-//            // 2 Try to save fakeValue under cacheKey.
-//            var returnedCachValue2 = ((ICache)cache).GetOrAdd(cacheKey, () =>
-//                {
-//                    return fakeValue;
-//                });
-//            #endregion
+            // Make sure the key we're about to test is empty
+            ((ICache)cache).Remove(cacheKey);
+            #endregion
 
 
-//            #region Assert
-//            // returnedCachValue1 should equal cacheValue because the cache is empty.
-//            Assert.AreEqual(cacheValue, returnedCachValue1);
+            #region Act
+            // 1 Try to save cacheValue under cacheKey.
+            //   Since this key is new, the cacheValue should be saved to the cache
+            //   and returned to returnedCachValue1;
+            var returnedCachValue1 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+                {
+                    return cacheValue;
+                });
 
-//            // returnedCachValue2 should equal cacheValue because the cache exists, 
-//            // so fakeValue was never returned.
-//            Assert.AreEqual(cacheValue, returnedCachValue2);
-//            Assert.AreNotEqual(fakeValue, returnedCachValue2);
-//            #endregion
-//        }
-
-//        [Test]
-//        [Continuous, Integration]
-//        [TestCase("InMemoryCacheTest:Remove:test-key", "test-value-1", "test-value-2")]
-//        [TestCase("InMemoryCacheTest:Remove:test-key", "", "test-value-2")]
-//        [TestCase("", "test-value-1", "test-value-2")]
-//        public void Remove(string cacheKey, string cacheValue1, string cacheValue2)
-//        {
-//            #region Arrange
-//            // Set up some variables
-//            var cache = new InMemoryCache();
-
-//            // Make sure the key we're about to test is empty
-//            ((ICache)cache).Remove(cacheKey);
-//            #endregion
+            // 2 Try to save fakeValue under cacheKey.
+            var returnedCachValue2 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+                {
+                    return fakeValue;
+                });
+            #endregion
 
 
-//            #region Act
-//            // 1 Try to save cacheValue under cacheKey.
-//            //   Since this key is new, the cacheValue should be saved to the cache
-//            //   and returned to returnedCachValue1;
-//            var returnedCachValue1 = ((ICache)cache).GetOrAdd(cacheKey, () =>
-//            {
-//                return cacheValue1;
-//            });
+            #region Assert
+            // returnedCachValue1 should equal cacheValue because the cache is empty.
+            Assert.AreEqual(cacheValue, returnedCachValue1);
 
-//            // 2 Remove the cacheKey
-//            ((ICache)cache).Remove(cacheKey);
+            // returnedCachValue2 should equal cacheValue because the cache exists, 
+            // so fakeValue was never returned.
+            Assert.AreEqual(cacheValue, returnedCachValue2);
+            Assert.AreNotEqual(fakeValue, returnedCachValue2);
+            #endregion
+        }
 
-//            // 3 Try to save cacheValue2 under cacheKey again
-//            var returnedCachValue2 = ((ICache)cache).GetOrAdd(cacheKey, () =>
-//            {
-//                return cacheValue2;
-//            });
-//            #endregion
+        [Test]
+        [Continuous, Integration]
+        [TestCase("InMemoryCacheTest:Remove:test-key", "test-value-1", "test-value-2")]
+        [TestCase("InMemoryCacheTest:Remove:test-key", "", "test-value-2")]
+        [TestCase("", "test-value-1", "test-value-2")]
+        public void Remove(string cacheKey, string cacheValue1, string cacheValue2)
+        {
+            #region Arrange
+            // Set up some variables
+            var cache = new InMemoryCache();
+
+            // Make sure the key we're about to test is empty
+            ((ICache)cache).Remove(cacheKey);
+            #endregion
 
 
-//            #region Assert
-//            // returnedCachValue2 should equal cacheValue2 (but not cacheValue1)
-//            // because the cache was empty the second time.
-//            Assert.AreNotEqual(cacheValue1, returnedCachValue2);
-//            Assert.AreEqual(cacheValue2, returnedCachValue2);
-//            #endregion
-//        }
-//    }
-//}
+            #region Act
+            // 1 Try to save cacheValue under cacheKey.
+            //   Since this key is new, the cacheValue should be saved to the cache
+            //   and returned to returnedCachValue1;
+            var returnedCachValue1 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+            {
+                return cacheValue1;
+            });
+
+            // 2 Remove the cacheKey
+            ((ICache)cache).Remove(cacheKey);
+
+            // 3 Try to save cacheValue2 under cacheKey again
+            var returnedCachValue2 = ((ICache)cache).GetOrAdd(cacheKey, () =>
+            {
+                return cacheValue2;
+            });
+            #endregion
+
+
+            #region Assert
+            // returnedCachValue2 should equal cacheValue2 (but not cacheValue1)
+            // because the cache was empty the second time.
+            Assert.AreNotEqual(cacheValue1, returnedCachValue2);
+            Assert.AreEqual(cacheValue2, returnedCachValue2);
+            #endregion
+        }
+    }
+}
