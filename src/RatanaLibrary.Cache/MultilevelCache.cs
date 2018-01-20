75b4b225e277ef64310091fcc7308921116335fe
Karn Ratana <karn.ratana@gmail.com>
2018-01-19 23:31:51 -0500

    Add RatanaLibrary.Cache/MultilevelCache.cs

---
+++ src/RatanaLibrary.Cache/MultilevelCache.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RatanaLibrary.Cache
+{
+    /// <summary>
+    /// An implementation of the ICache interface that does not do anything.
+    /// This is useful for testing.
+    /// </summary>
+    public class MultilevelCache : IMultilevelCache
+    {
+        /// <summary>
+        /// This is the list of caches to be used, 
+        /// in the order that was received via the constructor.
+        /// </summary>
+        protected IList<ICache> Caches { get; set; }
+
+        public MultilevelCache(params ICache[] caches)
+        {
+            this.Caches = caches.ToList();
+        }
+
+        T IMultilevelCache.GetOrAdd<T>(string key, Func<T> orAdd, params TimeSpan[] expiration)
+        {
+            // Validate expiration array.  Make sure each cache level is accounted for.
+            if (expiration.Length != this.Caches.Count)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "expiration",
+                    "The number of cache expirations must match the number of Caches.");
+            }
+
+            // This variable tells us up to which level of the caches need to be updated.
+            // The default is 0; meaning the key is found in first level cache, 
+            // therefore nothing needs to be updated.
+            int cacheLevelNeedingUpdate = 0;
+
+            // Set the value to default.
+            T value = default(T);
+
+            // Loop through the caches until the key is found
+            foreach (ICache cache in this.Caches)
+            {
+                if (cache.TryGet(key, out value))
+                {
+                    break;
+                }
+
+                cacheLevelNeedingUpdate++;
+            }
+
+            // If the value is still default, then the key was not found.
+            // Call orAdd() to get the value.
+            if (EqualityComparer<T>.Default.Equals(value, default(T)))
+            {
+                value = orAdd();
+            }
+
+            int ii = 0;
+            // Save the value back into the caches that need them.
+            // Since the value was not found via TryGet() above for some caches,
+            // calling GetOrAdd() on those will trigger their orAdd().
+            // Wrap the value in an anonymous function and pass it in as orAdd().
+            foreach (ICache cache in this.Caches)
+            {
+                if (ii >=  cacheLevelNeedingUpdate)
+                {
+                    break;
+                }
+
+                cache.GetOrAdd(key, () => value, expiration[ii]);
+            }
+
+
+            return value;
+        }
+
+        T ICache.GetOrAdd<T>(String key, Func<T> orAdd, TimeSpan expiration)
+        {
+            // If one expiration is passed in, treat it as only one level
+            return ((IMultilevelCache)this).GetOrAdd(key, orAdd, expiration);
+        }
+
+        void ICache.Remove(String key)
+        {
+            foreach (ICache cache in this.Caches)
+            {
+                cache.Remove(key);
+            }
+        }
+
+        Boolean ICache.TryGet<T>(string key, out T value)
+        {
+            value = default(T);
+            foreach (ICache cache in this.Caches)
+            {
+                if (cache.TryGet(key, out value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ tests/Tests.RatanaLibrary/Cache/MultilevelCacheTest.cs
@@ -0,0 +1,142 @@
+﻿using Moq;
+using NUnit.Framework;
+using RatanaLibrary.Cache;
+using System;
+using Tests.RatanaLibrary.Attributes;
+
+namespace Tests.RatanaLibrary.Common.Cache
+{
+    [TestFixture]
+    public class MultilevelCacheTest
+    {
+        [Test]
+        [Continuous, Integration]
+        [TestCase("MultilevelCacheTest:GetOrAdd:test-key1", "test-value", "test-fake-value")]
+        [TestCase("MultilevelCacheTest:GetOrAdd:test-key2", "", "test-fake-value")]
+        [TestCase("", "test-value", "test-fake-value")]
+        public void GetOrAdd(string cacheKey, string cacheValue, string fakeValue)
+        {
+            #region Arrange 
+            // Set up some variables
+            var cacheLevel1 = new Mock<ICache>();
+            var cacheLevel2 = new Mock<ICache>();
+            var cacheLevel3 = new Mock<ICache>();
+
+            // These caches' GetOrAdd() should return cacheValue
+            Func<String> orAdd1 = () => cacheValue;
+            cacheLevel1.Setup(x => x.GetOrAdd(cacheKey, orAdd1, TimeSpan.MaxValue)).Returns(cacheValue);
+            cacheLevel2.Setup(x => x.GetOrAdd(cacheKey, orAdd1, TimeSpan.MaxValue)).Returns(cacheValue);
+            cacheLevel3.Setup(x => x.GetOrAdd(cacheKey, orAdd1, TimeSpan.MaxValue)).Returns(cacheValue);
+
+            // These caches' TryGet() should return cacheValue
+            cacheLevel1.Setup(x => x.TryGet(cacheKey, out cacheValue)).Returns(true);
+            cacheLevel2.Setup(x => x.TryGet(cacheKey, out cacheValue)).Returns(true);
+            cacheLevel3.Setup(x => x.TryGet(cacheKey, out cacheValue)).Returns(true);
+
+            IMultilevelCache cache = new MultilevelCache(
+                cacheLevel1.Object,
+                cacheLevel2.Object,
+                cacheLevel3.Object
+            );
+
+
+            // Make sure the key we're about to test is empty
+            cache.Remove(cacheKey);
+            #endregion
+
+
+            #region Act
+            // 1 Try to save cacheValue under cacheKey.
+            //   Since this key is new, the cacheValue should be saved to the cache
+            //   and returned to returnedCachValue1;
+            var returnedCachValue1 = cache.GetOrAdd(cacheKey, () =>
+                {
+                    return cacheValue;
+                },
+                TimeSpan.MaxValue,
+                TimeSpan.MaxValue,
+                TimeSpan.MaxValue);
+
+            // 2 Try to save fakeValue under cacheKey.
+            // Since the key already exist, fakeValue is never reached.
+            var returnedCachValue2 = cache.GetOrAdd(cacheKey, () =>
+                {
+                    return fakeValue;
+                },
+                TimeSpan.MaxValue,
+                TimeSpan.MaxValue,
+                TimeSpan.MaxValue);
+            #endregion
+
+
+            #region Assert
+            // returnedCachValue1 should equal cacheValue because the cache was empty.
+            Assert.AreEqual(cacheValue, returnedCachValue1);
+
+            // returnedCachValue2 should equal cacheValue because the cache exists, 
+            // so fakeValue was never returned.
+            Assert.AreEqual(cacheValue, returnedCachValue2);
+            Assert.AreNotEqual(fakeValue, returnedCachValue2);
+            #endregion
+        }
+
+        [Test]
+        [Continuous, Integration]
+        [TestCase("MultilevelCacheTest:Remove:test-key", "test-value-1", "test-value-2")]
+        [TestCase("MultilevelCacheTest:Remove:test-key", "", "test-value-2")]
+        [TestCase("", "test-value-1", "test-value-2")]
+        public void Remove(string cacheKey, string cacheValue1, string cacheValue2)
+        {
+            #region Arrange
+            // Set up some variables
+            var cacheLevel1 = new Mock<ICache>();
+            var cacheLevel2 = new Mock<ICache>();
+            var cacheLevel3 = new Mock<ICache>();
+
+            IMultilevelCache cache = new MultilevelCache(
+                cacheLevel1.Object,
+                cacheLevel2.Object,
+                cacheLevel3.Object
+            );
+
+
+            // Make sure the key we're about to test is empty
+            cache.Remove(cacheKey);
+            #endregion
+
+
+            #region Act
+            // 1 Try to save cacheValue under cacheKey.
+            //   Since this key is new, the cacheValue should be saved to the cache
+            //   and returned to returnedCachValue1;
+            var returnedCachValue1 = cache.GetOrAdd(cacheKey, () =>
+                {
+                    return cacheValue1;
+                }, 
+                TimeSpan.MaxValue, 
+                TimeSpan.MaxValue, 
+                TimeSpan.MaxValue);
+
+            // 2 Remove the cacheKey
+            cache.Remove(cacheKey);
+
+            // 3 Try to save cacheValue2 under cacheKey again
+            var returnedCachValue2 = cache.GetOrAdd(cacheKey, () =>
+                {
+                    return cacheValue2;
+                },
+                TimeSpan.MaxValue,
+                TimeSpan.MaxValue,
+                TimeSpan.MaxValue);
+            #endregion
+
+
+            #region Assert
+            // returnedCachValue2 should equal cacheValue2 (but not cacheValue1)
+            // because the cache was empty the second time.
+            Assert.AreNotEqual(cacheValue1, returnedCachValue2);
+            Assert.AreEqual(cacheValue2, returnedCachValue2);
+            #endregion
+        }
+    }
+}
